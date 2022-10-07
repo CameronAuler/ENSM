d2f70c9a22cf3f5b201caa826fa49ba7e8d6d5c3
Cameron Auler <cameron.auler@mymail.champlain.edu>
2022-10-07 09:21:36 -0400

    Fixing server and client scripts

--- Code/client.py
+++ Code/client.py
@@ -1,9 +1,11 @@
-#client.py
 import socket
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "localhost" # "127.0.1.1"
-port = 8000
-my_socket.connect((host, port))
-while True:
-    message_to_send = input("Enter your message : ")
-    my_socket.send(message_to_send.encode())
+
+server_port = 8000
+server_address = "10.0.0.215"
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket.connect((server_address, server_port))
+
+socket.send(f"testing client --> server {server_address} connection.".encode('utf-8'))
+print(socket.recv(1024))
--- Code/server.py
+++ Code/server.py
@@ -1,12 +1,32 @@
-#server.py
+
 import socket
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-PORT = 8000
-ADDRESS = "0.0.0.0"
-my_socket.bind((ADDRESS, PORT))
-my_socket.listen()
-client, client_address = my_socket.accept()
-# Instead of receiving only one message, let's make an infinite loop
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connection information
+server_port = 8000
+server_address = "10.0.0.215"
+
+# The following line of code retrieves the IP of the system it is on automatically, but doesn't work with VM initialization
+'''
+ADDRESS = socket.gethostbyname(socket.gethostbyname())
+'''
+
+# Bind the port and address
+socket.bind((server_address, server_port))
+
+# Initiate listening on server
+# Restricts the number of connections with the 5(ignores excess connection requests)
+server_socket.listen(5)
+
+# Comm_socket is another socket for communication on the server, the server_socket only accepts connections.
 while True:
-    result = client.recv(1024)
-    print(result.decode())
+    comm_socket, client_address = server_socket.accept()
+    print(f"Connected to {client_address}")
+    
+    # 1024 is the buffer size in bytes
+    message = comm_socket.recv(1024).decode('utf-8')
+    print(f"Message from client --> {message}")
+    comm_socket.send(f"server has recieved your message!".encode('utf-8'))
+    comm_socket.close()
+    print(f"connection with {client_address} terminated!")
